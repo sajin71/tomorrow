6ee5b1b1ce32a55761baa779d53ebc04d8a0f6c9
shuyoko <shuyoko@gmail.com>
2013-11-09 18:43:03 +0900

    testsimu

--- convx86/tools.cpp
+++ convx86/tools.cpp
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#ifdef NO_DPRINTF
+void dprintf(const char *str, ...) { return; }
+#else
 void dprintf(const char *str, ...) {
 	va_list arg;
 	va_start(arg, str);
 	vfprintf(stderr, str, arg);
 	va_end(arg);
 }
+#endif
 
 int getregbyorder(int order, int regmax, const int oporder[]) {
 	for(int r=0; r<regmax; r++) {
