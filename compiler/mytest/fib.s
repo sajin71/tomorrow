448091ba89b653db17a4846de63c3124a3f19098
tomo <halfmount.tomo@gmail.com>
2013-11-09 13:46:47 +0900

    new fib

--- compiler/mytest/fib.s
+++ compiler/mytest/fib.s
@@ -1,32 +1,20 @@
 .section	".rodata"
 .align	8
 .section	".text"
-	lli $5, 0x4d
-	sw $5, -1($0)
-
-	lli $29, 1000
 	jal	min_caml_start
 	halt
 min_caml_print_int:
-	lli $5, 0x50
-	sw $5, -1($0)
-
-	sw $1, -1($0)
+	sw $1 ,-1($0)
 	jr $31
+	nop
 fib.10:
-	lli $5, 0x46
-	sw $5, -1($0)
-
-	sw $1, -1($0)
 	lli	$2, 1
-	addi	$27, $2, 0
-	slt	$27, $27, $1
+	slt	$27, $2, $1
 	bne	$27, $0, ble_else.24
+	nop
 	jr $31
+	nop
 ble_else.24:
-	lli $5, 0x42
-	sw $5, -1($0)
-
 	lli	$2, 1
 	sub	$2, $1, $2
 	sw	$1, 0($29)
@@ -49,11 +37,9 @@ ble_else.24:
 	lw	$2, 4($29)
 	add	$1, $2, $1
 	jr $31
+	nop
 min_caml_start:
-	lli $5, 0x53
-	sw $5, -1($0)
-
-	lli	$1, 3
+	lli	$1, 10
 	sw	$31, 4($29)
 	addi	$29, $29, 8
 	jal	fib.10
