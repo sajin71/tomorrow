fd1d3af86c4b3e4c8bdcfff6eaf7c2378bcf0dfe
shuyoko <shuyoko@gmail.com>
2013-12-03 18:51:08 +0900

    add testcase: shootout/mandelbrot, shootout/harmonic

--- compiler/shootout/harmonic.ml
+++ compiler/shootout/harmonic.ml
@@ -1,7 +1,7 @@
 (*MINCAML*) let rec f n i d s =
 (*MINCAML*)   if i > n then s else
 (*MINCAML*)   f n (i + 1) (d +. 1.0) (s +. 1.0 /. d) in
-(*MINCAML*) print_int (int_of_float (1000000.0 *. f 100000000 2 2.0 1.0));
+(*MINCAML*) print_int (int_of_float ( floor(100000.0 *. f 1000 2 2.0 1.0)) );
 (*NOMINCAML let f n = *)
 (*NOMINCAML   let s = ref 1.0 in *)
 (*NOMINCAML   let d = ref 2.0 in *)
