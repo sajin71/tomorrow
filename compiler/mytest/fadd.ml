3ea5b38947a0c17a72e88a35ad088d82d97f4c2c
shuyoko <shuyoko@gmail.com>
2013-12-10 20:16:18 +0900

    for fadd test

---
+++ compiler/mytest/fadd.ml
@@ -0,0 +1,37 @@
+print_xfloat (3.0 +. 4.0);
+print_newline ();
+
+print_xfloat (1.5 +. 6.9);
+print_newline ();
+
+print_xfloat (1.0 +. 1.0);
+print_newline ();
+
+print_xfloat (8.0 +. 1000.0);
+print_newline ();
+
+print_xfloat (6.22 +. 1.3);
+print_newline ();
+
+
+
+print_xfloat (3.0 -. 4.0);
+print_newline ();
+
+print_xfloat (1.5 -. 6.9);
+print_newline ();
+
+print_xfloat (1.0 -. 1.0);
+print_newline ();
+
+print_xfloat (8.0 -. 1000.0);
+print_newline ();
+
+print_xfloat (6.22 -. 1.3);
+print_newline ();
+
+
+
+
+
+print_int(0)
