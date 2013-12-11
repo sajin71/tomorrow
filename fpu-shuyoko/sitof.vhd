662f9103ec91b00c2c30754bb01aa21a84677c30
shuyoko <shuyoko@gmail.com>
2013-12-11 18:27:29 +0900

    sitof: BUG FIX: expoの足し算で上位ビットが消えていた問題を修正

--- fpu-shuyoko/sitof.vhd
+++ fpu-shuyoko/sitof.vhd
@@ -22,7 +22,7 @@ end component;
   signal inabs    : std_logic_vector(30 downto 0);
   signal expo     : std_logic_vector(4 downto 0);
   signal shifted  : std_logic_vector(29 downto 0);
-  signal expocarry: std_logic;
+  signal fraccarry: std_logic;
   
   
 -- for stage2
@@ -32,7 +32,7 @@ end component;
   
   signal rounded2   : std_logic_vector(23 downto 0);
   signal expoiszero2: std_logic;
-  signal expocarry2 : std_logic;
+  signal fraccarry2 : std_logic;
   
 begin
   
@@ -46,26 +46,26 @@ begin
   
   shifted(29 downto 0) <= SHL(inabs(29 downto 0), not expo); -- "not expo" means 31-expo; MSB always '1', push out
   
-  expocarry <= shifted(6) and (shifted(7) or shifted(5) or shifted(4) or shifted(3) 
+  fraccarry <= shifted(6) and (shifted(7) or shifted(5) or shifted(4) or shifted(3) 
                                                     or shifted(2) or shifted(1) or shifted(0));
 
   latch: process (clk)
   begin
     if rising_edge(clk) then
-	  signbit2   <= i(31);
-      expocarry2 <= expocarry;
+      signbit2   <= i(31);
+      fraccarry2 <= fraccarry;
       expo2      <= expo;
       shifted2   <= shifted(29 downto 7);
     end if;
   end process latch;
 
-  rounded2 <= ('0'&shifted2) + expocarry2;
-  expoiszero2 <= '1' when expo2(4 downto 0)="00000" else '0';
+  rounded2 <= ('0'&shifted2) + fraccarry2;
+  expoiszero2 <= '1' when expo2="00000" else '0';
   
   f(31) <= signbit2; -- sign
   f(30 downto 23) <= "00000000" when signbit2='0' and expoiszero2='1' else
                      "10011110" when expoiszero2='1' else
-                     expo2 + ("0111111" & rounded2(23)); -- expo: frac carried: +127, otherwise: +126
+                     ("000"&expo2) + ("0111111" & rounded2(23)); -- expo: frac carried: +127, otherwise: +126
   f(22 downto 0) <= rounded2(22 downto 0); --frac
 
 end RTL;
