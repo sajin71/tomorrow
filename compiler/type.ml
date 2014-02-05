bbc3378c79677347e096c845e92318be0a496ac1
tomo <halfmount.tomo@gmail.com>
2014-02-05 23:27:25 +0900

    added t_to_string method

--- compiler/type.ml
+++ compiler/type.ml
@@ -10,6 +10,22 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
 
+let rec t_to_string = function
+  | Unit -> "Unit"
+  | Bool -> "Bool"
+  | Int -> "Int"
+  | Float -> "Float"
+  | Fun(ts, typ) -> "Fun((" ^ typ_lst_to_string ts ^ ")," ^ t_to_string typ ^ ")"
+  | Tuple(ts) -> "Tuple(" ^ typ_lst_to_string ts ^ ")"
+  | Array(x) -> "Array(" ^ t_to_string x ^ ")"
+  | Var(x) -> "Var(" ^ match !x with None -> "None" | Some(y) -> t_to_string y ^ ")"
+and typ_lst_to_string lst =
+    let rec typ_lst_rec_to_string lst result = match lst with
+      | [] -> result
+      | x::[] -> result ^ t_to_string x
+      | x::xs -> typ_lst_rec_to_string xs (result ^ t_to_string x ^ ",") in
+    typ_lst_rec_to_string lst ""
+    
 open Format
 (*型をプリント 改行しない*)
 let rec print_type_t type_t =
