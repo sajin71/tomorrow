f2cea198bc29bc39b7eeb08021c65afcc4624629
tomo <halfmount.tomo@gmail.com>
2014-02-14 19:43:08 +0900

    added subexpression elimination

--- compiler/main.ml
+++ compiler/main.ml
@@ -3,7 +3,7 @@ let limit = ref 1000
 let rec iter n e = (* 最適化処理をくりかえす (caml2html: main_iter) *)
   Format.eprintf "iteration %d@." n;
   if n = 0 then e else
-  let e' = Elim.f (ConstFold.f (Inline.f (Assoc.f (Beta.f e)))) in
+  let e' = (Elim.f (Subexpr.f (ConstFold.f (Inline.f (Assoc.f (Beta.f e)))))) in
   if e = e' then e else
   iter (n - 1) e'
 
@@ -16,10 +16,11 @@ let lexbuf outchan l = (* バッファをコンパイルしてチャンネルへ
 	  (Virtual.f
 	     (Closure.f
 		(iter !limit
+        (Subexpr.f
 		   (Alpha.f
 		      (KNormal.f
 			 (Typing.f
-			    (Parser.exp Lexer.token l)))))))))
+			    (Parser.exp Lexer.token l))))))))))
 
 let string s = lexbuf stdout (Lexing.from_string s) (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
 
