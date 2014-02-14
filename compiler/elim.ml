117d2b399c34f3f85ab9678ba8d84b26a3e26fb5
tomo <halfmount.tomo@gmail.com>
2014-02-14 19:42:36 +0900

    moved function effect

--- compiler/elim.ml
+++ compiler/elim.ml
@@ -1,11 +1,5 @@
 open KNormal
 
-let rec effect = function (* 副作用の有無 (caml2html: elim_effect) *)
-  | Let(_, e1, e2) | IfEq(_, _, e1, e2) | IfLE(_, _, e1, e2) -> effect e1 || effect e2
-  | LetRec(_, e) | LetTuple(_, _, e) -> effect e
-  | App _ | Put _ | ExtFunApp _ -> true
-  | _ -> false
-
 let rec f = function (* 不要定義削除ルーチン本体 (caml2html: elim_f) *)
   | IfEq(x, y, e1, e2) -> IfEq(x, y, f e1, f e2)
   | IfLE(x, y, e1, e2) -> IfLE(x, y, f e1, f e2)
