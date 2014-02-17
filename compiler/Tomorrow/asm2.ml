347e5381c6143f75b228471fd2ef732e69a905c0
tomo <halfmount.tomo@gmail.com>
2014-02-17 16:18:13 +0900

    mli file for new data type

--- compiler/Tomorrow/asm2.ml
+++ compiler/Tomorrow/asm2.ml
@@ -1,3 +1,8 @@
+type ret =
+    | Value of (Id.t * Type.t) option
+    | Cls of Id.t * Id.t list * Id.t list
+    | Dir of Id.l * Id.t list * Id.t list
+
 type inst =
     | Nop
     | Set of Id.t * int
@@ -21,12 +26,18 @@ type inst =
     | FDiv of Id.t * Id.t * Id.t
     | LWC of Id.t * Id.t * id_or_imm
     | SWC of Id.t * Id.t * id_or_imm
+    (* virtual instructions *)
+    | IfEq of Id.t * Id.t * int (* shows where the else block starts *)
+    | IfLE of Id.t * Id.t * int
+    | IfGE of Id.t * Id.t * int
+    | IfFEq of Id.t * Id.t * int
+    | IfFLE of Id.t * Id.t * int
     | Comment of string
-    | CallCls of Id.t * Id.t list * Id.t list
-    | CallDir of Id.l * Id.t list * Id.t list
+    | CallCls of Id.t * Id.t list * Id.t list * Id.t (* dest *)
+    | CallDir of Id.l * Id.t list * Id.t list * Id.t (* dest *)
     | Save of Id.t * Id.t
     | Restore of Id.t * Id.t
-    | Return of (Id.t * Type.t) option
+    | Return of ret 
 
 type t = inst list
 
@@ -62,18 +73,123 @@ and g' prev = function
     | NonTail(x), Asm.FSub(y, z) -> FSub(x, y, z) :: prev
     | NonTail(x), Asm.FMul(y, z) -> FMul(x, y, z) :: prev
     | NonTail(x), Asm.FDiv(y, z) -> FDiv(x, y, z) :: prev
+    | NonTail(x), Asm.LWC(y, z') -> LWC(x, y, z') :: prev
+    | NonTail(_), Asm.SWC(x, y, z') -> SWC(x, y, z') :: prev
     | NonTail(x), Asm.Comment(s) -> Comment(s) :: prev
     | NonTail(_), Asm.Save(x, y) -> Save(x, y) :: prev (* won't be neccesarry *)
     | NonTail(x), Asm.Restore(y) -> Restore(x, y) :: prev (* won't be neccesarry *)
-    | Tail, (Asm.Nop | Asm.SW _ | Asm.SWC _ | Asm.Comment _ | Asm.Save _ as inst) ->
-        Return(None) :: g' prev (NonTail(Id.gentmp Type.Unit), inst)
+    | Tail, (Asm.Nop | Asm.SW _ | Asm.SWC _ | Asm.Comment _ | Asm.Save _ as exp) ->
+        Return(Value(None)) :: g' prev (NonTail(Id.gentmp Type.Unit), exp)
     | Tail, (Asm.Set _ | Asm.SetL _ |  Asm.Mov _ | Asm.Neg _ | Asm.Add _ | Asm.Sub _ |Asm.Mul _ | Asm.Div _ | Asm.SLL _ | Asm.SRL _ | Asm.LW _ 
-        as inst) -> let tmp = Id.gentmp Type.Int in
-            Return(tmp, Type.Int) :: g' prev (NonTail(tmp), inst)
-    | Tail, (Asm.FMov _ | Asm.FNeg _ | Asm.SetCLV _  | Asm.FAdd _ | Asm.FSub _ | Asm.FMul _ | Asm.FDiv _  | Asm.LWC _ as inst) -> let tmp = Id.gentmp Type.Float in
-            Return(tmp, Type.Float) :: g' prev (NonTail(tmp), inst)
+        as exp) -> let tmp = Id.gentmp Type.Int in
+            Return(Value(tmp, Type.Int)) :: g' prev (NonTail(tmp), exp)
+    | Tail, (Asm.FMov _ | Asm.FNeg _ | Asm.SetCLV _  | Asm.FAdd _ | Asm.FSub _ |
+    Asm.FMul _ | Asm.FDiv _  | Asm.LWC _ as exp) -> let tmp = Id.gentmp Type.Float in
+            Return(Value(tmp, Type.Float)) :: g' prev (NonTail(tmp), exp)
     | Tail, (Asm.Restore(x) as exp) -> (* won't be neccesarry *)
             prev
+    | Tail, Asm.IfEq(x, y, e1, e2) ->
+            let cont, else_start = g'_tail_if e1 e2 in
+            cont @ [IfEq(x, y, else_start)] @ prev
+    | Tail, Asm.IfLE(x, y, e1, e2) ->
+            let cont, else_start = g'_tail_if e1 e2 in
+            cont @ [IfLE(x, y, else_start)] @ prev
+    | Tail, Asm.IfGE(x, y, e1, e2) ->
+            let cont, else_start = g'_tail_if e1 e2 in
+            cont @ [IfGE(x, y, else_start)] @ prev
+    | Tail, Asm.IfFEq(x, y, e1, e2) ->
+            let cont, else_start = g'_tail_if e1 e2 in
+            cont @ [IfFEq(x, y, else_start)] @ prev
+    | Tail, Asm.IfFLE(x, y, e1, e2) ->
+            let cont, else_start = g'_tail_if e1 e2 in
+            cont @ [IfFLE(x, y, else_start)] @ prev
+    | NonTail(z), Asm.IfEq(x, y, e1, e2) ->
+            let cont, else_start = g'_non_tail_if z e1 e2 in
+            cont @ [IfEq(x, y, else_start)] @ prev
+    | NonTail(z), Asm.IfLE(x, y, e1, e2) ->
+            let cont, else_start = g'_non_tail_if z e1 e2 in
+            cont @ [IfLE(x, y, else_start)] @ prev
+    | NonTail(z), Asm.IfGE(x, y, e1, e2) ->
+            let cont, else_start = g'_non_tail_if z e1 e2 in
+            cont @ [IfGE(x, y, else_start)] @ prev
+    | NonTail(z), Asm.IfFEq(x, y, e1, e2) ->
+            let cont, else_start = g'_non_tail_if z e1 e2 in
+            cont @ [IfFEq(x, y, else_start)] @ prev
+    | NonTail(z), Asm.IfFLE(x, y, e1, e2) ->
+            let cont, else_start = g'_non_tail_if z e1 e2 in
+            cont @ [IfFLE(x, y, else_start)] @ prev
+    | Tail, Asm.CallCls(x, ys, zs) -> Return(Cls(x, ys, zs)) :: prev
+    | Tail, Asm.CallDir(l, ys, zs) -> Return(Dir(x, ys, zs)) :: prev
+    | NonTail(a), Asm.CallCls(x, ys, zs) -> CallCls(x, ys, zs, a) :: prev
+    | NonTail(a), Asm.CallDir(l, ys, zs) -> CallDir(l, ys, zs, a) :: prev
+and g'_tail_if e1 e2 =
+    let true_list = g' [] (Tail, e1) in
+    let false_list = g' [] (Tail, e2) in
+    let cont = false_list @ true_list in
+    let else_start = List.length true_list + 1 in
+    (cont, else_start)
+and g'_non_tail_if z e1 e2 =
+    let true_list = g' [] (NonTail(z), e1) in
+    let false_list = g' [] (NonTail(z), e2) in
+    let cont = false_list @ true_list in
+    let else_start = List.length true_list + 1 in
+    (cont, else_start)
+
+let rec string_of_id_list = function
+    | [] -> "" 
+    | x::[] -> x
+    | x::xs -> x ^ ", " ^ string_of_id_list xs
+
+let print_inst = function
+    | Nop -> print_string "Nop\n"
+    | Set(x, v) -> print_string ("Set " ^ x ^ ", " ^ string_of_int v ^ "\n")
+    | SetL(x, Id.L(l)) -> print_string ("SetL " ^ x ^ ", " ^ "L(" ^ l ^ ")\n")
+    | SetCLV(x, l) -> print_string ("SetCLV " ^ x ^ ", " ^ l ^ "\n")
+    | Mov(x, y) -> print_string ("Mov " ^ x ^ ", " ^ y ^ "\n")
+    | Neg(x) -> print_string ("Neg " ^ x ^ "\n")
+    | Add(x, y, Asm.V(z)) -> print_string ("Add " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | Add(x, y, Asm.C(z)) -> print_string ("Add " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | Sub(x, y, z) -> print_string ("Sub " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | Mul(x, y, Asm.V(z)) -> print_string ("Mul " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | Mul(x, y, Asm.C(z)) -> print_string ("Mul " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | Div(x, y, Asm.V(z)) -> print_string ("Div " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | Div(x, y, Asm.C(z)) -> print_string ("Div " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | SLL(x, y, Asm.V(z)) -> print_string ("SLL " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")  
+    | SLL(x, y, Asm.C(z)) -> print_string ("SLL " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | SRL(x, y, Asm.V(z)) -> print_string ("SRL " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | SRL(x, y, Asm.C(z)) -> print_string ("SRL " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | LW(x, y, Asm.V(z)) -> print_string ("LW " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | LW(x, y, Asm.C(z)) -> print_string ("LW " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | SW(x, y, Asm.V(z)) -> print_string ("SW " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | SW(x, y, Asm.C(z)) -> print_string ("SW " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | FMov(x, y) -> print_string ("FMov " ^ x ^ ", " ^ y ^ "\n")
+    | FNeg(x, y) -> print_string ("FNeg " ^ x ^ ", " ^ y ^ "\n")
+    | FAdd(x, y, z) -> print_string ("FAdd " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | FSub(x, y, z) -> print_string ("FSub " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | FMul(x, y, z) -> print_string ("FMul " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | FDiv(x, y, z) -> print_string ("FDiv " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | LWC(x, y, Asm.V(z)) -> print_string ("LWC " ^ x ^ ", " ^ y ^ ", " ^ z ^ "\n")
+    | LWC(x, y, Asm.C(z)) -> print_string ("LWC " ^ x ^ ", " ^ y ^ ", " ^ string_of_int z ^ "\n")
+    | IfEq(x, y, else_start) -> print_string ("IfEq " ^ x ^ ", " ^ y ^ "else_start: " ^ string_of_int else_start ^ "\n") 
+    | IfLE(x, y, else_start) -> print_string ("IfLE " ^ x ^ ", " ^ y ^ "else_start: " string_of_int else_start ^ "\n")
+    | IfGE(x, y, else_start) -> print_string ("IfGE " ^ x ^ ", " ^ y ^ "else_start: " ^ string_of_int else_start ^ "\n")
+    | IfFEq(x, y, else_start) -> print_string ("IfFEq " ^ x ^ ", " ^ y ^ "else_start: " ^ string_of_int else_start ^ "\n")
+    | IfFLE(x, y, else_start) -> print_string ("IfFLE " ^ x ^ ", " ^ y ^ "else_start: " ^ string_of_int else_start ^ "\n")
+    | Comment(comment) -> print_string ("Comment " ^ comment ^ "\n")
+    | CallCls(x, ys, zs, dest) -> print_string ("CallCls " ^ x ^ ", (" ^ string_of_id_list ys ^ "), (" ^ string_of_id_list zs ^ "), dest: " ^ dest ^ "\n")  
+    | CallDir(x, ys, zs, dest) -> print_string ("CallDir " ^ x ^ ", (" ^ string_of_id_list ys ^ "), (" string_of_id_list zs ^ "), dest: " ^ dest ^ "\n")
+    | Save(x, y) -> print_string ("Save " ^ x ^ ", " ^ y ^ "\n")
+    | Restore(x, y) -> print_string ("Restore " ^ x ^ ", " ^ y ^ "\n")
+    | Return(Value(None)) -> print_string "Return None\n"
+    | Return(Value(x, typ)) -> (match typ with
+                                | Type.Int -> print_string "Return " ^ x ^ " : int\n"
+                                | Type.Float -> print_string "Return " ^ x ^ " : float\n"
+                                | _ -> print_string "Return " ^ x ^ "\n") 
+    | Return(Cls(x, ys, zs)) -> print_string ("Return Cls(" ^ x ", (" ^ string_of_id_list ys ^ "), (" ^ string_of_id_list zs ^ "))")
+    | Return(Dir(x, ys, zs)) -> print_string ("Return Dir(" ^ x ", (" ^ string_of_id_list ys ^ "), (" ^ string_of_id_list zs ^ "))")
+
+
+
 
 
 
---
+++ compiler/Tomorrow/asm2.mli
@@ -0,0 +1,52 @@
+type ret = 
+    | Value of (Id.t * Type.t) option
+    | Cls of Id.t * Id.t list * Id.t list
+    | Dir of Id.t * Id.t list * Id.t list
+
+type inst =
+    | Nop
+    | Set of Id.t * int
+    | SetL of Id.t * Id.l
+    | SetCLV of Id.t * Id.t
+    | Mov of Id.t * Id.t
+    | Neg of Id.t
+    | Add of Id.t * Id.t * Asm.id_or_imm
+    | Sub of Id.t * Id.t * Id.t
+    | Mul of Id.t * Id.t * Asm.id_or_imm
+    | Div of Id.t * Id.t * Asm.id_or_imm
+    | SLL of Id.t * Id.t * Asm.id_or_imm
+    | SRL of Id.t * Id.t * Asm.id_or_imm
+    | LW of Id.t * Id.t * Asm.id_or_imm
+    | SW of Id.t * Id.t * Asm.id_or_imm
+    | FMov of Id.t * Id.t
+    | FNeg of Id.t * Id.t
+    | FAdd of Id.t * Id.t * Id.t
+    | FSub of Id.t * Id.t * Id.t
+    | FMul of Id.t * Id.t * Id.t
+    | FDiv of Id.t * Id.t * Id.t
+    | LWC of Id.t * Id.t * id_or_imm
+    | SWC of Id.t * Id.t * id_or_imm
+    (* virtual instructions *)
+    | IfEq of Id.t * Id.t * int (* shows where the else block starts *)
+    | IfLE of Id.t * Id.t * int
+    | IfGE of Id.t * Id.t * int
+    | IfFEq of Id.t * Id.t * int
+    | IfFLE of Id.t * Id.t * int
+    | Comment of string
+    | CallCls of Id.t * Id.t list * Id.t list * Id.t (* dest *)
+    | CallDir of Id.l * Id.t list * Id.t list * Id.t (* dest *)
+    | Save of Id.t * Id.t
+    | Restore of Id.t * Id.t
+    | Return of ret 
+
+type t = inst list
+
+type fundef = {name : Id.l; args : Id.t list; fargs : Id.t list; body : t; ret : Type.t }
+
+type prog = Prog of (Id.l * float) list * fundef list * t
+
+type dest = Tail | NonTail of Id.t
+
+val f : Asm.prog -> prog
+
+val print_prog : prog -> unit
