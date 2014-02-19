3fb74940067ba813c45d8bd24a1a55ca1c300a4d
tomo <halfmount.tomo@gmail.com>
2014-02-19 16:18:16 +0900

    minor fix

--- compiler/Tomorrow/emit.ml
+++ compiler/Tomorrow/emit.ml
@@ -14,17 +14,17 @@ let save x =
     if not (List.mem x !stackmap) then
         stackmap := !stackmap @ [x]
 
-let savef x =
+(* let savef x =
     stackset := S.add x !stackset;
     if not (List.mem x !stackmap) then
         (let pad =
             if List.length !stackmap mod 2 = 0 then [] else [Id.gentmp Type.Int] in 
         stackmap := !stackmap @ pad @ [x; x])
-
+*)
 let locate x =
     let rec loc = function
         | [] -> []
-        | y :: zs when x = y -> 0 :: List.map succ(loc zs)
+        | y :: zs when x = y -> 0 :: List.map succ (loc zs)
         | y :: zs -> List.map succ (loc zs) in
         loc !stackmap
 
