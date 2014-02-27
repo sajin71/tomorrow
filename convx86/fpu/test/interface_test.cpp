e50ebd97c84646895b5760f89ec5bfb268b4a4c7
Hiroaki Mikami <hiroaki8270@mail.goo.ne.jp>
2014-02-27 16:53:55 +0900

    tmp commit

--- convx86/fpu/test/interface_test.cpp
+++ convx86/fpu/test/interface_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "interface.h"
+#include <bitset>
 
 union t {
     uint32_t i;
@@ -8,25 +9,24 @@ union t {
 
 TEST(InterfaceTest, FaddSimpleTest){
     t a, b, c;
-    a.f = b.f = 0;
-    c.i = _fadd(a.i, b.i);
-    EXPECT_EQ(0, c.f);
-
-    a.f = 1; 
-    c.i = _fadd(a.i, b.i);
-    EXPECT_EQ(1, c.f);
-
-    b.f = 1;
-    c.i = _fadd(a.i, b.i);
-    EXPECT_EQ(2, c.f);
-
-    b.f = -1;
-    c.i = _fadd(a.i, b.i);
-    EXPECT_EQ(0, c.f);
-
-    b.f = -100;
-    c.i = _fadd(a.i, b.i);
-    EXPECT_EQ(-99, c.f);
+    for(int i = 0; i <= 0xffffffff; i++){
+	for(int j = i; j <= 0xffffffff; j++){
+	    t d;
+	    a.i = i;
+	    b.i = j;
+	    d.f = a.f+b.f;
+	    c.i = _fadd(a.i, b.i);
+	    //c.f = a.f+b.f;
+	    std::bitset<32> t = d.i ^ c.i;
+	    int cnt = 0;
+	    for(int i = 0; i < 32; i++){
+		if(t[i]){
+		    cnt ++;
+		}
+	    }
+	    EXPECT_GE(1, cnt);
+	}
+    }
 }
 TEST(InterfaceTest, FmulSimpleTest){
     t a, b, c;
